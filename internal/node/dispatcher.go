cd6ac07a13bd5c49c6977fd27e48f3ff20224b87
baltej223 <baltej963@gmail.com>
2026-06-14 17:53:58 +0530

    Added Concurrency

--- internal/node/dispatcher.go
+++ internal/node/dispatcher.go
@@ -16,30 +16,30 @@ func Dispatch(msg transport.ParsedMessage, me *Node) {
 
 	switch msg.Type {
 	case transport.PING:
-		handlePing(msg, me)
+		go handlePing(msg, me)
 	case transport.PONG:
-		handlePong(msg, me)
+		go handlePong(msg, me)
 	case transport.JOIN:
-		handleJoin(msg, me)
+		go handleJoin(msg, me)
 	case transport.JOIN_ACK:
-		handleJoinACK(msg, me)
+		go handleJoinACK(msg, me)
 	case transport.GOSSIPMEMBERSHIP:
-		handleMembership(msg, me)
+		go handleMembership(msg, me)
 	case transport.GET:
-		handleGet(msg, me)
+		go handleGet(msg, me)
 	case transport.GET_RESPONSE:
-		handleGetResponse(msg, me)
+		go handleGetResponse(msg, me)
 	case transport.GET_REJ:
-		handleGetREJ(msg, me)
+		go handleGetREJ(msg, me)
 	case transport.PUT:
-		handlePut(msg, me)
+		go handlePut(msg, me)
 	case transport.PUT_ACK:
-		handlePutACK(msg, me)
+		go handlePutACK(msg, me)
 	case transport.PUT_REJ:
-		handlePutREJ(msg, me)
+		go handlePutREJ(msg, me)
 	case transport.SYNC_MEMBERSHIP:
-		handleSYNCMembership(msg, me)
+		go handleSYNCMembership(msg, me)
 	case transport.SYNC_MEMBERSHIP_RESPONSE:
-		handleSYNCMembershipResponse(msg, me)
+		go handleSYNCMembershipResponse(msg, me)
 	}
 }
